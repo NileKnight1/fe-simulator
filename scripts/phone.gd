d1442c9ff686b594378bed7a2a0c85cb69aa839c
NileKnight0 <ammartube777@gmail.com>
2026-04-04 02:40:57 +0200

    completed narrating system and first scene and started second scene

--- scripts/phone.gd
+++ scripts/phone.gd
@@ -4,7 +4,6 @@ extends Node2D
 @onready var chat = $Control/Label
 @onready var option1 = $Control/Control/option1
 @onready var option2 = $Control/Control/option2
-@onready var girl = $AnimatableBody2D2/AnimatedSprite2D
 
 var not_now = 1
 var cur = 0
@@ -48,43 +47,16 @@ func scene(op):
 	
 	match cur:
 		1:
-			opp(op, "Hi!", "What?")
-			girl.flip_h = 1
-			girl.offset.x = 0
-		2:
-			not_now = 1
-			sp(next_message, 2)
-			show_op()
-			opts("What's time now?", "You're beautiful.")
-		3:
-			opp(op, "Don't you have a watch!", "Yes I know that.")
-			temp_op = op
-		4:
-			if temp_op == 1:
-				not_now = 1
-				sp(next_message, 2)
-				show_op()
-				opts("I have but when I saw you I lost myself.", "Nevermind.")
-			else:
-				sp("Yes I know that.", 2)
-				scene(1)
-		6:
-			sp("I'm going", 2)
-			
-		7:
-			girl.visible = 0
-			sp("Bruh.", 1)
-		8:
-			get_tree().change_scene_to_file("res://scenes/phone.tscn")
-
+			pass
+		
 
 
 
 func _on_option_1_pressed() -> void: 
 	scene(1)
-	sp(option1.text, 1)
-	hide_op()
-	not_now = 0
+	#sp(option1.text, 1)
+	#hide_op()
+	#not_now = 0
 	
 func _on_option_2_pressed() -> void: 
 	scene(2)
@@ -100,3 +72,7 @@ func _input(event: InputEvent) -> void:
 		print(cur)
 		scene(1)
 	
+
+
+func _on_chatgpt_pressed() -> void:
+	$chatgpt.visible = 0
